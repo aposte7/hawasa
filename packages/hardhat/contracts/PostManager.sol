ef7b12c894daa6d7cc3a953ec2e198c171f277f4
aposte7 <olman4jesus@gmail.com>
2025-12-24 10:26:57 +0300

    feat: add PostManager and ProfileManager contracts from https://github.com/aposte7/web3-social-media

---
+++ packages/hardhat/contracts/PostManager.sol
@@ -0,0 +1,249 @@
+//SPDX-License-Identifier: MIT
+pragma solidity >=0.8.0 <0.9.0;
+
+import {ProfileManager} from "./ProfileManager.sol";
+
+contract PostManager {
+
+    uint private postIdCount;
+    uint private commentIdCounter;
+
+    ProfileManager profileManager;
+
+    constructor(address _profileManagerAddress) {
+        profileManager = ProfileManager(_profileManagerAddress);
+    }
+
+
+    struct Comment {
+        uint commentId;
+        uint postId;
+        uint authorId;
+        string content;
+        uint createdAt;
+        bool isDeleted;
+    }
+
+    struct Post { 
+        uint ownerId;
+        uint postId;
+        string content;
+        uint createdAt;
+        bool isDeleted;
+    }
+
+    struct PostLocation {
+        uint ownerId;     
+        uint index;        
+    }
+
+
+    mapping(uint256 userId => uint256[] userPostsIndex) private userPosts; 
+    mapping(uint256 postId => PostLocation PostLocationStruct) private postIndex;
+    mapping(uint256 postId => mapping(uint256 userId => bool)) private postLikes;
+    mapping(uint256 postId => uint) private postLikeCount;
+    mapping(uint256 commentId => Comment) private comments; 
+    mapping(uint256 postId => uint256[] commentId) private postComments; 
+
+    Post[] private allPosts;
+
+    event PostCreated( uint indexed postId, uint indexed ownerId, address indexed ownerAddress, string content, uint createdAt);
+    event PostEdited( uint indexed postId, string newContent, uint editedAt);
+    event PostDeleted( uint indexed postId, uint deletedAt);
+    event PostLiked( uint indexed postId, uint indexed userId, address indexed userAddress, uint createdAt);
+    event PostUnliked( uint indexed postId, uint indexed userId, address indexed userAddress, uint removedAt);
+    event CommentCreated( uint indexed commentId, uint indexed postId, uint indexed authorId, address authorAddress, string content, uint createdAt);
+    event CommentEdited( uint indexed commentId, string newContent, uint editedAt);
+    event CommentDeleted( uint indexed commentId, uint deletedAt);
+
+
+
+    function createPost(string calldata _content) external {
+
+        require(bytes(_content).length >  3, "content should be atleast letter 3");
+        uint ownerId = profileManager.getProfileId(msg.sender);
+
+        postIdCount++;
+        uint newPostId = postIdCount;
+
+        allPosts.push();
+        uint index = allPosts.length - 1;
+        Post storage newPost = allPosts[index];
+
+        newPost.ownerId = ownerId;
+        newPost.postId = newPostId;
+        newPost.content = _content;
+        newPost.createdAt = block.timestamp;
+        userPosts[ownerId].push(index);
+        postIndex[newPostId] = PostLocation(ownerId, index);
+
+        emit PostCreated(newPostId, ownerId, msg.sender, _content,  newPost.createdAt);
+    }
+
+    function getPostById(uint _postId) external view returns(Post memory){
+        require(_postExists(_postId), "Post doesn't exist");
+        PostLocation memory loc = postIndex[_postId];
+        return allPosts[loc.index];
+    }
+
+    function getPostsByUser(uint userId) external view returns (Post[] memory) {
+
+        uint[] memory userPostIndices = userPosts[userId];
+        Post[] memory tempUserPosts = new Post[](userPostIndices.length);
+
+        for (uint i = 0; i < userPostIndices.length; i++) {
+            tempUserPosts[i] = allPosts[userPostIndices[i]];
+        }
+
+        return tempUserPosts;
+    }
+
+    function _postExists(uint _postId) internal view returns (bool){
+        PostLocation memory loc = postIndex[_postId];
+        if(loc.ownerId != 0 && !_isPostDeleted(_postId)) return true;
+        else return false;
+    }
+
+    function getTotalPostCount() external view returns (uint256) {
+        return allPosts.length;
+    }
+
+    function deletePost(uint _postId) external {
+        uint userId = profileManager.getProfileId(msg.sender);
+        require(_postExists(_postId), "Post does not exist");
+
+        PostLocation memory loc = postIndex[_postId];
+        require(loc.ownerId == userId, "Not post owner");
+
+        Post storage post = allPosts[loc.index];
+        require(!post.isDeleted, "Already deleted");
+        post.isDeleted = true;
+        emit PostDeleted(_postId, block.timestamp);
+
+    }
+
+    function _isPostDeleted(uint256 _postId) internal view returns(bool){
+        PostLocation memory loc = postIndex[_postId];
+        return allPosts[loc.index].isDeleted;
+    }
+
+    function editPost(uint _postId, string calldata newContent) external {
+        uint userId = profileManager.getProfileId(msg.sender);
+        require(_postExists(_postId), "Post doesn't exist");
+        PostLocation memory loc = postIndex[_postId];
+        require(loc.ownerId == userId, "you don't have this post");
+        require(bytes(newContent).length >  3, "content should be atleast letter 3");
+        allPosts[loc.index].content = newContent;
+
+        emit PostEdited(_postId, newContent, block.timestamp);
+    }
+
+
+    // -- COMMENT Functionalites //
+    function commentOnPost(uint256 _postId, string calldata _content) external {
+        uint256 authorId = profileManager.getProfileId(msg.sender);
+        require(_postExists(_postId), "Post does not exist");
+        require(bytes(_content).length > 0, "Empty comment");
+
+        commentIdCounter++;
+        uint256 newCommentId = commentIdCounter;
+
+        comments[newCommentId] = Comment({
+            commentId: newCommentId,
+            postId: _postId,
+            authorId: authorId,
+            content: _content,
+            createdAt: block.timestamp,
+            isDeleted: false
+        });
+
+        postComments[_postId].push(newCommentId);
+        emit CommentCreated(newCommentId, _postId, authorId, msg.sender, _content, comments[newCommentId].createdAt);
+    }
+
+    function editComment(uint256 _commentId, string calldata newContent) external {
+        uint256 userId = profileManager.getProfileId(msg.sender);
+        uint256 authorId = comments[_commentId].authorId;
+        require(userId == authorId, "You don't own this comment");
+        Comment storage comment =   comments[_commentId];
+        require(!comment.isDeleted, "Comment does not exsist");
+
+        comment.content = newContent;
+
+        emit CommentEdited(_commentId, newContent, block.timestamp);
+    }
+
+    function deleteComment(uint256 _commentId) external {
+        uint256 userId = profileManager.getProfileId(msg.sender);
+        uint256 authorId = comments[_commentId].authorId;
+        require(userId == authorId, "You don't own this comment");
+        Comment storage comment =   comments[_commentId];
+        require(!comment.isDeleted, "Comment does not exsist");
+
+        comment.isDeleted = true;      
+        emit CommentDeleted(_commentId, block.timestamp);   
+    }
+
+    function getCommentsByPost(uint256 _postId, uint256 _offset, uint256 _limit) external view returns (Comment[] memory) {
+        require(_postExists(_postId), "Post does not exist");
+
+        uint256 totalComments = postComments[_postId].length;
+
+        if (_offset >= totalComments) {
+            return new Comment[](0);
+        }
+
+        uint256 end = _offset + _limit;
+        if (end > totalComments) {
+            end = totalComments;
+        }
+
+        uint256 resultSize = end - _offset;
+        Comment[] memory paginatedComments = new Comment[](resultSize);
+
+        uint256 index = 0;
+        for (uint256 i = _offset; i < end; i++) {
+            uint256 commentId = postComments[_postId][i];
+            paginatedComments[index] = comments[commentId];
+            index++;
+        }
+
+        return paginatedComments;
+    }
+
+
+    function getPostCommentCount(uint256 _postId) external view returns (uint256) {
+        require(_postExists(_postId), "Post does not exist");
+        return postComments[_postId].length;
+    }
+
+    //--  LIKE Functionalities --//
+
+    function togglePostLike(uint _postId) external {
+        uint userId = profileManager.getProfileId(msg.sender);
+        require(_postExists(_postId), "Post doesn't exist");
+
+        if (postLikes[_postId][userId]) {
+            postLikes[_postId][userId] = false;
+            postLikeCount[_postId]--;
+            emit PostUnliked(_postId, userId, msg.sender , block.timestamp);
+        } else {
+            postLikes[_postId][userId] = true;
+            postLikeCount[_postId]++;
+             emit PostLiked(_postId, userId, msg.sender , block.timestamp);
+        }
+    }
+
+
+    function getPostLikeCount(uint _postId) external view returns (uint) {
+        require(_postExists(_postId), "Post doesn't exist");
+        return postLikeCount[_postId];
+    }
+
+    function hasUserLikedPost(uint _postId, uint _userId) external view returns (bool) {
+        require(_postExists(_postId), "Post doesn't exist");
+        return postLikes[_postId][_userId];
+    }
+
+
+}
---
+++ packages/hardhat/contracts/ProfileManager.sol
@@ -0,0 +1,130 @@
+//SPDX-License-Identifier: MIT
+pragma solidity >=0.8.0 <0.9.0;
+
+contract ProfileManager {
+    struct Profile {
+        uint256 id;
+        string username;
+        string bio;
+        string avatarURI;
+        address owner;
+        uint256 createdAt;
+        uint256 updatedAt;
+    }
+
+    mapping(address => Profile) private profiles;
+    mapping(address => bool) private hasProfile;
+    mapping(string => bool) private usernameTaken;
+    address[] private profileOwners;
+    uint256 private profileCounter;
+
+    address private immutable owner;
+
+    constructor() {
+        owner = msg.sender;
+    }
+
+
+    event ProfileCreated(uint256 indexed profileId,address indexed owner,string username,uint256 createdAt
+    );
+
+    event ProfileUpdated(uint256 indexed profileId,uint256 updatedAt
+    );
+
+
+    modifier onlyProfileOwner() {
+        require(hasProfile[msg.sender], "Profile does not exist");
+        _;
+    }
+    modifier onlyOwner() {
+        require(owner == msg.sender, "Only Owner is allowed");
+        _;
+    }
+
+
+    function createProfile(string calldata username,string calldata bio,string calldata avatarURI) external returns (Profile memory) {
+        require(!hasProfile[msg.sender], "Profile already exists");
+        require(bytes(username).length >= 3, "Username too short");
+        require(!usernameTaken[username], "Username already taken");
+
+        profileCounter++;
+
+        Profile memory newProfile = Profile({
+            id: profileCounter,
+            username: username,
+            bio: bio,
+            avatarURI: avatarURI,
+            owner: msg.sender,
+            createdAt: block.timestamp,
+            updatedAt: block.timestamp
+        });
+
+        profiles[msg.sender] = newProfile;
+        hasProfile[msg.sender] = true;
+        usernameTaken[username] = true;
+        profileOwners.push(msg.sender);
+
+        emit ProfileCreated(newProfile.id,msg.sender,username,block.timestamp
+        );
+
+        return newProfile;
+    }
+
+
+    function updateProfile(string calldata newUsername,string calldata newBio,string calldata newAvatarURI) external onlyProfileOwner returns (Profile memory) {
+
+        Profile storage user = profiles[msg.sender];
+
+        if (keccak256(bytes(newUsername)) != keccak256(bytes(user.username))) {
+            require(bytes(newUsername).length >= 3, "Username too short");
+            require(!usernameTaken[newUsername], "Username already taken");
+
+            usernameTaken[user.username] = false;
+            usernameTaken[newUsername] = true;
+            user.username = newUsername;
+        }
+
+        user.bio = newBio;
+        user.avatarURI = newAvatarURI;
+        user.updatedAt = block.timestamp;
+
+        emit ProfileUpdated(user.id, block.timestamp);
+
+        return user;
+    }
+
+
+    function getProfile(address user) external view returns (Profile memory) {
+        require(hasProfile[user], "Profile not found");
+        return profiles[user];
+    }
+
+    function getMyProfile() external view onlyProfileOwner returns (Profile memory) {
+        require(hasProfile[msg.sender], "Profile not found");
+        return profiles[msg.sender];
+    }
+
+    function getProfileId(address userAddress) external view returns (uint256) {
+        require(hasProfile[userAddress], "Profile not found");
+        return profiles[userAddress].id;
+    }
+
+    function totalProfiles() external view onlyOwner returns (uint256) {
+        return profileOwners.length;
+    }
+
+    function getAllProfiles() external view onlyOwner returns (Profile[] memory) {
+        uint256 count = profileOwners.length;
+        Profile[] memory list = new Profile[](count);
+
+        for (uint256 i = 0; i < count; i++) {
+            list[i] = profiles[profileOwners[i]];
+        }
+
+        return list;
+    }
+
+    function profileExists(address user) external view returns (bool) {
+        return hasProfile[user];
+    }
+}
